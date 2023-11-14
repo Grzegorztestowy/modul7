cd34a8b961d7c40ace670c36e7dac6d39aca0510
Grzegorz Prabucki <132297553+Grzegorztestowy@users.noreply.github.com>
2023-11-14 20:15:54 +0100

    copying files from previous work

---
+++ README.md
@@ -0,0 +1,20 @@
+# Grzegorz Prabucki - Lista zadań
+## ^_^
+
+[demo](https://grzegorztestowy.github.io/dev/modul6)
+
+## Welcome to the Readme :-)
+I am presenting another project made for educational purposes.
+This time it is a task list in which you can mark completed tasks and delete entries.
+
+Presentation :
+
+![presentation](img/modul6present.png)
+
+
+Used technologies:
+- HTML
+- CSS
+- JavaScript
+- BEM
+- Normalize
---
+++ js/script.js
@@ -0,0 +1,87 @@
+{
+    const tasks = [];
+  
+    const addNewTask = (newTaskContent) => {
+      tasks.push({ content: newTaskContent });
+      render();
+    };
+  
+    const toggleTaskDone = (taskIndex) => {
+      tasks[taskIndex].done = !tasks[taskIndex].done;
+      render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+      };
+  
+    const bindEvents = () => {
+      
+      const toggleDoneButtons = document.querySelectorAll(".js-done");
+  
+      toggleDoneButtons.forEach((toggleDoneButton, index) => {
+        toggleDoneButton.addEventListener("click", () => {
+          toggleTaskDone(index);
+        });
+      });
+
+      const removeButtons = document.querySelectorAll(".js-remove");
+  
+      removeButtons.forEach((removeButtons, index) => {
+        removeButtons.addEventListener("click", () => {
+          removeTask(index);
+        });
+      });
+
+    };
+  
+    const render = () => {
+      let tasksListHTMLContent = "";
+  
+      for (const task of tasks) {
+        tasksListHTMLContent += `
+          <li class="tasks__list">
+            <button class="js-done tasks__buttons tasks__buttons--done">
+             ${task.done ? "✔" : " "}
+            </button>
+            <span class="task__item${task.done ? " task__item--done" : ""}">
+             ${task.content}
+            </span>
+            <button class="js-remove tasks__buttons tasks__buttons--remove">🗑</button>
+          </li>
+        `;
+      }
+  
+      document.querySelector(".js-tasks").innerHTML = tasksListHTMLContent;
+  
+      bindEvents();
+    };
+  
+  
+    const onFormSubmit = (event) => {
+      event.preventDefault();
+  
+      const newTaskElement = document.querySelector(".js-newTask");
+      const newTaskContent = newTaskElement.value.trim();
+  
+      if (newTaskContent !== "") {
+        addNewTask(newTaskContent);
+        newTaskElement.value = "";
+      }
+  
+      newTaskElement.focus();
+    };
+  
+  
+    const init = () => {
+      render();
+      const form = document.querySelector(".js-form");
+      form.addEventListener("submit", onFormSubmit);
+  
+    };
+  
+    init();
+  }
+  
+  
